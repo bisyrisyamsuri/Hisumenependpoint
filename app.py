dc5c18dd4dd7cabfc420670c0479d5f3e0577602
bisyrisyamsuri <syamsuribisyri@gmail.com>
2022-08-24 18:28:39 +0700

    pirst tapi apdet

--- app.py
+++ app.py
@@ -39,14 +39,53 @@ def detail_by_id(detail_id):
     if result > 0:
         detail = cur.fetchone()
         return jsonify({'data': detail}, 200)
-    
-@app.route('/search', methods=['GET', 'POST'])
-def search_by_id(search_id):
-    cur = mysql.connection.cursor()
-    result = cur.execute("SELECT * FROM wisata WHERE nama LIKE %s OR wilayah LIKE %s", search_id)
-    if result > 0:
-        search = cur.fetchall()
-        return jsonify({'data': search}, 200)    
 
+@app.route('/rekomendasi', methods=['GET'])
+def rekomendasi_by_id():
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM wisata where rekomendasi = 'Y'")
+    if result > 0:
+        rekomendasi = cur.fetchall()
+        return jsonify({'data': rekomendasi}, 200)
+
+    
+# @app.route('/search', methods=['GET', 'POST'])
+# def search_by_id(search_id):
+#     cur = mysql.connection.cursor()
+#     result = cur.execute("SELECT * FROM wisata WHERE nama LIKE %s OR wilayah LIKE %s", search_id)
+#     if result > 0:
+#         search = cur.fetchall()
+#         return jsonify({'data': search}, 200)    
+@app.route('/search', methods=['GET'])
+def search():
+    nama = args.get('nama')
+    wilayah = args.get('wilayah')
+    if None not in (nama, wilayah):
+        result = {key: value for key, value in wisata.items() if key == nama and value == wilayah}
+    elif nama is not None:
+        result = {key: value for key, value in wisata.items() if key == nama}
+    elif wilayah is not None:
+        result = {key: value for key, value in wisata.items() if value == wilayah}
+    return result
+
+@app.route('/register', methods=['POST'])
+def register():
+        user = request.form
+        username = user['username']
+        password = user['password']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO user(username, password, role) VALUES(%s, %s, 2)",(username, password))
+        mysql.connection.commit()
+        cur.close()
+        return jsonify({'data': user}, 200)
+    
+@app.route('/profil/<profil_id>', methods=['GET'])
+def profil(profil_id):
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM user where id = %s", profil_id)
+    if result > 0:
+        jenis = cur.fetchall()
+        return jsonify({'data': jenis}, 200)
+    
 if __name__ == '__main__':
     app.run(debug=True)
